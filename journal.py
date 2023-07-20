8568e046a524a21d9fc65bce548a15fa3ddc61b9
KramerSE <94350296+KramerOsborn@users.noreply.github.com>
2023-07-19 18:12:49 -0700

    from collections import OrderedDict
    ordered_dict = OrderedDict()
    my_dict = dict()
    
    Townspeople = {
        'Cecilia': {
            'Gender': 'Female',
            'Marriage': 'Yes',
            'Likes': ['Rare Crop 7', 'Star Milk S', 'Star Milk SS', 'Sashimi', 'Meuniere Set', 'Nizaka Gozen',
                     'Strawberry Cake', 'Mushroom Curry', 'Mushroom Gratin', 'Milk Soup', 'Sushi', 'Flowers',
                     'Hitogata Haniwa',
                     'Horse Doll', 'Gekko Seki'],
            'Dislikes': ['Rare Crop 4', 'Rare Crop 18', 'Cucumber', 'Unknown Dish',
                         'Fish Stew', 'Pickles', 'Kabayaki Don', 'Dig Site Relics'],
        },
        'Lumina': {
            'Gender': 'Female',
            'Marriage': 'Yes',
            'Love': ['Sweet Potato Soup', 'Egg', 'S rank Cheese', 'Trick Blue Flower',
                     'S rank Melon', 'Passionbloom Flower', 'Moonlight Ore'],
            'Likes': ['Golden Wool', 'Flowers'],
            'Dislikes': ['S Ranked Butter', 'Fish'],
        },
        'Nami': {
            'Gender': 'Non-Binary',
            'Marriage': 'Yes',
            'Love': ['Trick Blue Flower', 'Clay Figurine', 'Leaf Fossil', 'S Rank Melon', 'Curry'],
            'Likes': ['Milky Soup', 'Egg Soup', 'Fossils'],
            'Dislikes': ['All Flowers except Trick Blue Flowers', 'Golden Wool'],
        },
        'Molly': {
            'Gender': 'Female',
            'Marriage': 'Yes',
            'Love': ['Butter', 'Golden Wool'],
            'Likes': ['B or higher ranked Butter', 'B or higher ranked Cheese', 'Flowers', 'Blue Melon S', 'Melon S',
                     'Rare Crop 7 S', 'Rare Crop 8 S', 'Large Spotted Char', 'Curry', 'Tataaro Stir-fry', 'Fried Tataaro',
                     'Crispy Tarte', 'Egg Soup', 'Starry Sky Pie', 'Irogo no mi', 'Coin', 'Silver Coin',
                     'Gold Coin', 'Gekkou Seki',
                     'Moonlight Ore', 'Chitose Stone', 'Normal Milk S'],
            'Dislikes': ['Large King Masu', 'Large Bucketmouth Brass', 'Failed Dish', 'Fish Stew', 'Konsaibaise',
                         'Vegetable Juice', 'Mamedon', 'Yakuzen Soup', 'Portucci', 'Fertilizer', 'Fodder', 'Animal Treat',
                         'Dig Site Things'],
            'Hates': ['Large Amur Catfish'],
        },
        'Matthew': {
            'Gender': 'Male',
            'Marriage': 'Yes',
            'Love': ['Heartwarming Soup', 'Lovely Smoothie', 'Sweet Smoothie', 'Smoothie', 'Mushroom Curry', 'Kabayaki',
                    'Grilled Fish', 'Crops S'],
            'Likes': ['Milk', 'Eggs', 'Root Crops', 'Cooked Dishes'],
            'Hated': ['Tempura','Kakiage Tempura', 'Minced Veggie Bowl', 'Butter'],
    
        },
    
        'Gordy': {
            'Gender': 'Male',
            'Marriage': 'Yes',
            'Loves': ['Cheese S', 'Melon S', 'Milk S'],
            'Likes': ['Flowers', 'Eggs'],
            'Dislike': ['Super Sashimi', "Lou's Spices"],
            'Hated': ['Fish'],
        },
    
        'Gustafa': {
            'Gender': 'Male',
            'Marriage': 'Yes',
            'Loves': ['Melon S', 'Milk S*', 'Super Sashimi', 'Cheese (S*)', 'Egg', 'PassionBloom', 'Flower'],
            'Likes': ['Toy Flower'],
            'Dislikes': ['Marinade', 'Large Amur Catfish', 'Golden Wool'],
        },
    
        'Rock': {
        'Gender': 'Male',
        'Marriage': 'Yes',
        'Loves': [],
        'Likes': [],
        'Dislikes': ['Fish']
    
        }

---
+++ journal.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+ordered_dict = OrderedDict()
+my_dict = dict()
+
+Townspeople = {
+    'Cecilia': {
+        'Gender': 'Female',
+        'Marriage': 'Yes',
+        'Like': ['Rare Crop 7', 'Star Milk S', 'Star Milk SS', 'Sashimi', 'Meuniere Set', 'Nizaka Gozen', 'Strawberry Cake', 'Mushroom Curry', 'Mushroom Gratin', 'Milk Soup', 'Sushi', 'Flowers', 'Hitogata Haniwa', 'Horse Doll', 'Gekko Seki'],
+        'Dislikes': ['Rare Crop 4', 'Rare Crop 18', 'Cucumber', 'Unknown Dish', 'Fish Stew', 'Pickles', 'Kabayaki Don', 'Dig Site Relics'],
+    },
+    'Lumina': {
+        'Gender': 'Female',
+        'Marriage': 'Yes',
+        'Love': ['Sweet Potato Soup', 'Egg', 'S rank Cheese', 'Trick Blue Flower', 'S rank Melon', 'Passionbloom Flower', 'Moonlight Ore'],
+        'Like': ['Golden Wool', 'Flowers'],
+        'Dislikes': ['S Ranked Butter', 'Fish'],
+    },
+    'Nami': {
+        'Gender': 'Non-Binary',
+        'Marriage': 'Yes',
+        'Love': ['Trick Blue Flower', 'Clay Figurine', 'Leaf Fossil', 'S Rank Melon', 'Curry'],
+        'Like': ['Milky Soup', 'Egg Soup', 'Fossils'],
+        'Dislikes': ['All Flowers except Trick Blue Flowers', 'Golden Wool'],
+    },
+}
